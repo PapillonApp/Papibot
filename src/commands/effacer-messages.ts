5b0548c0f54429ba157c69eadea7d65a640bfb3f
ryzenixx <96339570+ryzenixx@users.noreply.github.com>
2025-09-02 00:17:44 +0200

    feat: /effacer-messages command

---
+++ src/commands/effacer-messages.ts
@@ -0,0 +1,103 @@
+import {
+    ChatInputCommandInteraction,
+    ContainerBuilder,
+    GuildMember,
+    GuildTextBasedChannel,
+    MessageFlags,
+    TextDisplayBuilder,
+} from "discord.js";
+import { Command } from "../types/Command";
+import type { ExtendedClient } from "../types/ExtendedClient";
+import { hasStaffRole } from "../utils/roleChecker";
+import { errorMessage } from "../utils/errorMessage";
+import "dotenv/config";
+
+const clearCommand: Command = {
+    name: "effacer-messages",
+    description: "Effacer d'un seul coup un nombre précis de message dans un salon",
+    dm: false,
+    options: [
+        {
+            type: "integer",
+            name: "nombre",
+            description: "Nombre de messages à supprimer (entre 1 et 100)",
+            required: true,
+        },
+    ],
+
+    async run(bot: ExtendedClient, interaction: ChatInputCommandInteraction) {
+
+        // Récupération des variables
+        let numberOfMessages = interaction.options.getInteger("nombre");
+        const logsChannelId = process.env.CHANNEL_LOGS as string;
+        const logsChannel = interaction.guild?.channels.cache.get(logsChannelId);
+
+        // Vérification des permissions
+        if (!hasStaffRole(interaction.member as GuildMember)) {
+            await interaction.reply(
+                errorMessage(
+                    bot,
+                    "Désolé, tu n’as pas la permission d’utiliser cette commande.",
+                    "Permission refusée",
+                    "0x203",
+                    false
+                )
+            );
+            return;
+        }
+
+        // Vérification du nombre saisi
+        if (numberOfMessages || 0 < 1 || numberOfMessages || 0 > 100) {
+            await interaction.reply(
+                errorMessage(
+                    bot,
+                    "Le nombre de messages à supprimer doit être compris entre ``1`` et ``100`` !",
+                    "Demande invalide",
+                    "0x200",
+                    false
+                )
+            );
+            return;
+        }
+
+        const deletedMessages = await (interaction.channel as GuildTextBasedChannel).bulkDelete(numberOfMessages || 0, true);
+
+        // Envoyer un message de confirmation
+        const text = new TextDisplayBuilder().setContent(`${process.env.GREEN_PAPILLON} **Tu viens de supprimer ${deletedMessages.size} messages de ce salon !**`);
+
+        const container = new ContainerBuilder()
+            .addTextDisplayComponents(text);
+
+        // Envoyer dans le salon dans laquelle la commande a été faîte
+        interaction.reply({
+            flags: [MessageFlags.IsComponentsV2],
+            components: [container],
+            allowedMentions: {
+                parse: [],
+                roles: [],
+            },
+        });
+
+        // Envoyer dans le salon des logs du serveur
+        const text2 = new TextDisplayBuilder().setContent(`${process.env.GREEN_PAPILLON} **<@${interaction.member?.user.id}> a supprimer ${deletedMessages.size} messages du salon <#${interaction.channel?.id}>**`);
+
+        const container2 = new ContainerBuilder()
+            .addTextDisplayComponents(text2);
+
+        if (logsChannel && logsChannel.isTextBased()) {
+            logsChannel.send({
+                flags: [MessageFlags.IsComponentsV2],
+                components: [container2],
+                allowedMentions: {
+                    parse: [],
+                    roles: [],
+                },
+            });
+        } else {
+            return;
+        }
+
+    },
+};
+
+export default clearCommand;
