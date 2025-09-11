5f8e5046c1cece3a1abd0e771f81a393954dcea0
ryzenixx <96339570+ryzenixx@users.noreply.github.com>
2025-09-11 23:10:08 +0200

    fix(ts-build): error

--- src/utils/ensureDatabase.ts
+++ src/utils/ensureDatabase.ts
@@ -2,15 +2,15 @@ import type { ExtendedClient } from "../types/ExtendedClient";
 import loadDatabase from "../loaders/loadDatabase";
 
 export async function ensureDatabase(bot: ExtendedClient): Promise<boolean> {
-    if (bot.db) return true; // déjà connecté
+    if (bot.db) return true;
 
     try {
         bot.db = loadDatabase();
 
         await new Promise<void>((resolve, reject) => {
-            bot.db!.connect((err) => {
+            bot.db!.query("SELECT 1", (err) => {
                 if (err) return reject(err);
-                console.log("🔄 Reconnexion DB réussie");
+                console.log("🔄 Connexion DB OK");
                 resolve();
             });
         });
