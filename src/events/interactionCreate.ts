4386974cd5958d551ef04399f4fb45517e9e27bb
Mael <96339570+ryzenixx@users.noreply.github.com>
2025-10-11 00:12:34 +0200

    feat: implementation of rule validation button management

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,7 +1,8 @@
 import { ExtendedClient } from "../types/extendedClient";
 import { errorMessage } from "../functions/errorMessage";
-import { Interaction } from "discord.js";
+import { ContainerBuilder, GuildMember, Interaction, MessageFlags, SectionBuilder, TextDisplayBuilder, ThumbnailBuilder } from "discord.js";
 import { Command } from "../types/command";
+import config from '../../config.json';
 
 export default async function interactionCreate(bot: ExtendedClient, interaction: Interaction): Promise<void> {
     if(interaction.isChatInputCommand()) {
@@ -18,6 +19,28 @@ export default async function interactionCreate(bot: ExtendedClient, interaction
         } catch (err) {
             await interaction.reply(errorMessage("Une erreur est survenue lors de l’exécution de la commande."));
             return console.error(err);
+        }
+    }
+
+    if(interaction.isButton()) {
+        if(interaction.customId === "acceptRules") {
+            if((interaction.member as GuildMember).roles.cache.has(config.server.roles.unverified || "")) {
+                const title = new TextDisplayBuilder().setContent(`## Bienvenue parmis nous ! ${config.emojis.papillon}`);
+                const description = new TextDisplayBuilder().setContent(`Tu as désormais accès à l'entièreté du serveur Discord de Papillon, bon séjour parmis nous ! 💚`);
+                const thumbnail = new ThumbnailBuilder({
+                    media: {
+                        url: ``,
+                    },
+                });
+                const section = new SectionBuilder().addTextDisplayComponents(title, description).setThumbnailAccessory(thumbnail);
+                const container = new ContainerBuilder().addSectionComponents(section);
+                
+                await (interaction.member as GuildMember).roles.remove(config.server.roles.unverified);
+                await interaction.reply({
+                    flags: [MessageFlags.IsComponentsV2, MessageFlags.Ephemeral],
+                    components: [container]
+                });
+            }
         }
     }
 }
