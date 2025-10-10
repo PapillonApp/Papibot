2722d8c60a8a7c78f3e36218d66a7f9e8c897218
Mael <96339570+ryzenixx@users.noreply.github.com>
2025-10-10 23:33:19 +0200

    feat: implement interactionCreate function to handle chat input commands

---
+++ src/events/interactionCreate.ts
@@ -0,0 +1,23 @@
+import { ExtendedClient } from "../types/extendedClient";
+import { errorMessage } from "../functions/errorMessage";
+import { Interaction } from "discord.js";
+import { Command } from "../types/command";
+
+export default async function interactionCreate(bot: ExtendedClient, interaction: Interaction): Promise<void> {
+    if(interaction.isChatInputCommand()) {
+        const command: Command | undefined = bot.commands.get(interaction.commandName);
+        if(!command) {
+            await interaction.reply(errorMessage("Commande introuvable. Vérifie le nom et réessaie."));
+            return;
+        }
+        
+        try {
+            if(command.run) {
+                await command.run(bot, interaction);
+            }
+        } catch (err) {
+            await interaction.reply(errorMessage("Une erreur est survenue lors de l’exécution de la commande."));
+            return;
+        }
+    }
+}
