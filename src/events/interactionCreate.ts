b01614ced95e5dee7d1b3cd215781297dd1d3674
Mael <96339570+ryzenixx@users.noreply.github.com>
2025-10-11 16:24:37 +0200

    feat: add modals for ticket type selection in interactionCreate event

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,6 +1,6 @@
 import { ExtendedClient } from "../types/extendedClient";
 import { errorMessage } from "../functions/errorMessage";
-import { ContainerBuilder, GuildMember, Interaction, MessageFlags, SectionBuilder, TextDisplayBuilder, ThumbnailBuilder } from "discord.js";
+import { ActionRowBuilder, ContainerBuilder, GuildMember, Interaction, LabelBuilder, MessageFlags, ModalBuilder, SectionBuilder, TextDisplayBuilder, TextInputBuilder, TextInputStyle, ThumbnailBuilder } from "discord.js";
 import { Command } from "../types/command";
 import config from '../../config.json';
 
@@ -40,6 +40,75 @@ export default async function interactionCreate(bot: ExtendedClient, interaction
                     flags: [MessageFlags.IsComponentsV2, MessageFlags.Ephemeral],
                     components: [container]
                 });
+            }
+        }
+    }
+
+    if(interaction.isStringSelectMenu()) {
+        if(interaction.customId === "selectTicketType") {
+            const selected = interaction.values[0];
+            switch(selected) {
+                case "problemTicketType":
+                    const modalTicketProblem = new ModalBuilder()
+                        .setCustomId("modalTicketProblem")
+                        .setTitle(`${config.emojis.papillon} Problème sur Papillon`);
+                    const inputModelProblem = new TextInputBuilder()
+                        .setCustomId("inputModelProblem")
+                        .setLabel("Quel est ton appareil ? 📱")
+                        .setStyle(TextInputStyle.Short)
+                        .setPlaceholder("iPhone 15 Pro")
+                        .setRequired(true);
+                    const inputVersionProblem = new TextInputBuilder()
+                        .setCustomId("inputVersionProblem")
+                        .setLabel("Quelle est ta version de Papillon ? 🦋")
+                        .setStyle(TextInputStyle.Short)
+                        .setPlaceholder("8.2.0")
+                        .setRequired(true);
+                    const inputDescriptionProblem = new TextInputBuilder()
+                        .setCustomId("inputVersionProblem")
+                        .setLabel("Décris brièvement ton problème ci-dessous ⬇️")
+                        .setStyle(TextInputStyle.Paragraph)
+                        .setPlaceholder("Je n'arrives pas à me connecter à mon compte PRONOTE car...")
+                        .setRequired(true);
+                    
+                    const actionRowProblem1 = new ActionRowBuilder<TextInputBuilder>().addComponents(inputModelProblem);
+                    const actionRowProblem2 = new ActionRowBuilder<TextInputBuilder>().addComponents(inputVersionProblem);
+                    const actionRowProblem3 = new ActionRowBuilder<TextInputBuilder>().addComponents(inputDescriptionProblem);
+                    modalTicketProblem.addComponents(actionRowProblem1, actionRowProblem2, actionRowProblem3);
+                    await interaction.showModal(modalTicketProblem);
+                break;
+
+                case "questionTicketType":
+                    const modalTicketQuestion = new ModalBuilder()
+                        .setCustomId("modalTicketQuestion")
+                        .setTitle(`🦋 Question sur l'appli`);
+                    const inputDescriptionQuestion = new TextInputBuilder()
+                        .setCustomId("inputDescriptionQuestion")
+                        .setLabel("Quelle question veux-tu nous poser ? 😎")
+                        .setStyle(TextInputStyle.Paragraph)
+                        .setPlaceholder("Je voudrais savoir si Papillon est...")
+                        .setRequired(true);
+                    
+                    const actionRowQuestion1 = new ActionRowBuilder<TextInputBuilder>().addComponents(inputDescriptionQuestion);
+                    modalTicketQuestion.addComponents(actionRowQuestion1);
+                    await interaction.showModal(modalTicketQuestion);
+                break;
+
+                case "otherTicketType":
+                    const modalTicketOther = new ModalBuilder()
+                        .setCustomId("modalTicketOther")
+                        .setTitle(`👨‍⚖️ Autres demandes`);
+                    const inputDescriptionOther = new TextInputBuilder()
+                        .setCustomId("inputDescriptionOther")
+                        .setLabel("Quelle est ta demande ? 😎")
+                        .setStyle(TextInputStyle.Paragraph)
+                        .setPlaceholder("J'aimerais que...")
+                        .setRequired(true);
+                    
+                    const actionRowOther1 = new ActionRowBuilder<TextInputBuilder>().addComponents(inputDescriptionOther);
+                    modalTicketOther.addComponents(actionRowOther1);
+                    await interaction.showModal(modalTicketOther);
+                break;
             }
         }
     }
