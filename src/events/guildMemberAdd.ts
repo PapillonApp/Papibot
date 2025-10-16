fdf83520d86eddae7144ef50e527a398ca3b9468
Mael <96339570+ryzenixx@users.noreply.github.com>
2025-10-16 09:35:22 +0200

    delete: remove guildMemberAdd event handler for role assignment

--- src/events/guildMemberAdd.ts
+++
@@ -1,8 +0,0 @@
-import { GuildMember } from "discord.js";
-import config from '../../config.json';
-
-export default async (member: GuildMember) => {
-    try {
-        member.roles.add(config.server.roles.unverified);
-    } catch { return; }
-}
